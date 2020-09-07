c541bc55124804b86350192d19a0dd3e05195dd2
Devansh Panthi <69043897+2709-devansh@users.noreply.github.com>
2020-09-07 15:44:07 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -6,7 +6,9 @@ class dustbin{
        this.body1 = Bodies.rectangle(1150,640,200,20,options);
        this.body2 = Bodies.rectangle(1050,600,20,100,options);
        this.body3 = Bodies.rectangle(1250,600,20,100,options);
-       World.add(world, this.body1, this.body2, this.body3);
+       World.add(world, this.body1);
+       World.add(world, this.body2);
+       World.add(world, this.body3);
     }
     display(){
         rectMode(CENTER);
